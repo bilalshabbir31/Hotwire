0000e7af3592bcb78eb5b9fa6a83d1f12450b809
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-06-23 12:15:55 +0500

    add update post turbo stream

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,6 +40,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+        format.turbo_stream
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
