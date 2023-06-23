1764b843867f2b37dce2fb39a66e81a58614656d
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-06-23 11:12:51 +0500

    integrate turbo stream

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.turbo_stream
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
